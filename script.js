43a97776a51226e58e54c7697323acc3c60274be
Bakii <tayolowosale@gmail.com>
2024-07-16 09:16:22 +0100

    Changes

--- script.js
+++ script.js
@@ -4,44 +4,36 @@ document.addEventListener("DOMContentLoaded", () => {
 
   form.addEventListener("submit", (e) => {
     e.preventDefault();
+
+    let isValid = true;
+    const messages = [];
+
+    const username = form.username.value.trim();
+    if (username.length < 3) {
+      isValid = false;
+      messages.push("Username must be at least 3 characters long.");
+    }
+
+    const email = form.email.value.trim();
+    if (!email.includes("@") || !email.includes(".")) {
+      isValid = false;
+      messages.push('Email must contain "@" and "."');
+    }
+
+    // Password Validation
+    const password = form.password.value.trim();
+    if (password.length < 8) {
+      isValid = false;
+      messages.push("Password must be at least 8 characters long.");
+    }
+
+    feedbackDiv.style.display = "block";
+    if (isValid) {
+      feedbackDiv.textContent = "Registration successful!";
+      feedbackDiv.style.color = "#28a745";
+    } else {
+      feedbackDiv.innerHTML = messages.join("<br>");
+      feedbackDiv.style.color = "#dc3545";
+    }
   });
-
-  const username = document.getElementById("username").value;
-  const email = document.getElementById("email").value;
-  const password = document.getElementById("password").value;
-
-  username.trim();
-  email.trim();
-  password.trim();
-
-  const isValid = true;
-
-  const messages = [
-    "There is a username error",
-    "This is an email error",
-    "This is a password error",
-  ];
-
-  if (username.length < 3) {
-    isValid = false;
-    messages[0];
-  }
-
-  if (!email && "@" && ".") {
-    isValid = false;
-    messages[1];
-  }
-
-  if (!password.length >= 8) {
-    isValid = false;
-    messages[2];
-  }
-
-  feedbackDiv.style.display = "block";
-
-  if (isValid === true) {
-    feedbackDiv.textContent = "Registration Successful";
-    feedbackDiv.style.color = "#28a745";
-  } else {
-  }
 });
