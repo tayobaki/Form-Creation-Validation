5962e060d6c88f609facdef6f5260505178376ac
Bakii <tayolowosale@gmail.com>
2024-07-16 09:18:46 +0100

    changes

--- script.js
+++ script.js
@@ -2,26 +2,26 @@ document.addEventListener("DOMContentLoaded", () => {
   const form = document.getElementById("registration-form");
   const feedbackDiv = document.getElementById("form-feedback");
 
-  form.addEventListener("submit", (e) => {
-    e.preventDefault();
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const username = document.getElementById("username").value.trim();
+    const email = document.getElementById("email").value.trim();
+    const password = document.getElementById("password").value.trim();
 
     let isValid = true;
     const messages = [];
 
-    const username = form.username.value.trim();
     if (username.length < 3) {
       isValid = false;
       messages.push("Username must be at least 3 characters long.");
     }
 
-    const email = form.email.value.trim();
     if (!email.includes("@") || !email.includes(".")) {
       isValid = false;
-      messages.push('Email must contain "@" and "."');
+      messages.push("Email must be a valid email address.");
     }
 
-    // Password Validation
-    const password = form.password.value.trim();
     if (password.length < 8) {
       isValid = false;
       messages.push("Password must be at least 8 characters long.");
